31580f2897ae5c143e0f1babed77999f08409af3
TheGag96 <thegag96@gmail.com>
2014-11-08 13:19:52 -0600

    add project files

---
+++ android/src/com/codeday/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.codeday.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.codeday.CodeDayGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CodeDayGame(), config);
+	}
+}
